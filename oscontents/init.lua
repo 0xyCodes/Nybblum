6a4bb2241426ad3d91a0e462694a6b0d56cebd82
0xyCodes <133032680+0xyCodes@users.noreply.github.com>
2023-05-10 18:45:51 +0300

    Added a require thing

--- oscontents/init.lua
+++ oscontents/init.lua
@@ -3,4 +3,15 @@ local bootaddr = computer.getBootAddress()
 local gpu = component.proxy(component.list("gpu")())
 local fs = component.proxy(bootaddr)
 
+computer.beep(750, 0.25)
+computer.beep(1000, 0.25)
 gpu.set(1, 1, "Welcome to Nybblum.")
+gpu.set(1, 2, "Booting from "..bootaddr)
+gpu.set(1, 3, "Loading require.lua...")
+
+if fs.exists("/packages/require.lua") then
+  local handle = fs.open("/packages/require.lua", "r")
+  load(fs.read(handle, math.huge))
+else
+  error("Module not found: require.lua")
+end
