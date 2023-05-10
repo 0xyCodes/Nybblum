9ae96395a81d3db319958359c444d514e2547506
0xyCodes <133032680+0xyCodes@users.noreply.github.com>
2023-05-10 18:29:10 +0300

    Create require.lua

---
+++ oscontents/packages/require.lua
@@ -0,0 +1,12 @@
+local fs = component.proxy(computer.getBootAddress())
+
+function require(modulename)
+	if fs.exists("/packages/"..modulename..".lua")
+		local handle = fs.open("/packages/"..modulename..".lua", "r")
+		load(fs.read(handle, math.huge))
+	else
+		error("Module not found: "..modulename..".lua")
+	end
+end
+
+return require()
