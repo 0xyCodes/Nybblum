8eb29c65f71c6580207ab4814260e6056ce1e035
0xyCodes <133032680+0xyCodes@users.noreply.github.com>
2023-05-10 07:03:10 +0300

    Basic boot address and filesystem things

--- oscontents/init.lua
+++ oscontents/init.lua
@@ -1,1 +1,6 @@
-goober
+local bootaddr = computer.getBootAddress()
+
+local gpu = component.proxy(component.list("gpu")())
+local fs = component.proxy(bootaddr)
+
+gpu.set(1, 1, "Welcome to Nybblum.")
